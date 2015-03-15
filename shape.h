e1c1241299f0f0014f79c99b80d12e803c516301
mpalumbo6 <mpalumbo@ufl.edu>
2015-03-15 12:33:17 -0400

    Update shape.h

--- shape.h
+++ shape.h
@@ -1,12 +1,28 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+#include <QColor>
+#include <QGraphicsItem>
 
-class shape
+class shape public 
 {
 public:
-    shape();
-    ~shape();
+    Shape(const QColor &color, int x, int y)
+
+    QRectF boundingRect() const Q_DECL_OVERRIDE;
+    QPainterPath shape()  const Q_DECL_OVERRIDE;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget) Q_DECL_OVERRIDE;
+
+    protected:
+    void mousePresentEvent(QGraphicsSceneMouseEvent *event) Q_DECL_OVERRIDE;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event); Q_DECL_OVERRIDE;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);Q_DECL_OVERRIDE;
+    
+    private:
+    int x;
+    int y;
+    QColor color;
+    QVector<QPointF> stuff;
 };
 
 #endif // SHAPE_H
